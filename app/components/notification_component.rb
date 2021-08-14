c0eeb805b9d9b86b1eaad97d8d0c4d434e51fe63
Max VelDink <max@audaciousaardvark.com>
2021-08-14 10:49:18 -0500

    Add some defaults to the Notification component

--- app/components/notification_component.rb
+++ app/components/notification_component.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 class NotificationComponent < ViewComponent::Base
+  def initialize(type: :warning, text: "")
+    @type = type
+    @text = text
+  end
 
+  def notification_classes
+    klasses = ["my-4"] << case @type
+    when :warning
+      "bg-yellow-200"
+    else
+      "bg-green-200"
+    end
+
+    klasses.join(" ")
+  end
 end
