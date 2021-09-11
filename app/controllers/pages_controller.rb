867a09d8086f0c70837a6da6b1a13a4202be0008
Max VelDink <max@audaciousaardvark.com>
2021-09-11 18:06:12 -0500

    Add buttons and routes for changing notification with full page render

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,17 @@
 class PagesController < ApplicationController
-  def home; end
+  def home
+    @message = "Warning! You are about to do something."
+  end
+
+  def hello
+    @message = "Hello there!"
+
+    render template: "pages/home"
+  end
+
+  def goodbye
+    @message = "Goodbye for now!"
+
+    render template: "pages/home"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/", to: "pages#home"
+
+  get "/hello", to: "pages#hello"
+  get "/goodbye", to: "pages#goodbye"
 end
